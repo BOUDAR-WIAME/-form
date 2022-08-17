9c5307bfbf9dd2abfc541dd90d38d62ffba4565d
BOUDAR-WIAME <110188081+BOUDAR-WIAME@users.noreply.github.com>
2022-08-17 18:01:08 +0100

    Update validation.js

--- validation.js
+++ validation.js
@@ -2,51 +2,107 @@ let D = new Date ();
 document.getElementById('D').innerHTML = 'Bonjour Mr/Mme la date est : ' + D.toLocaleDateString();
 
 function validateForm(){ 
+    //nom
     var x=document.getElementById("name").value;
     var y=document.getElementById("nn");
+    var letters = /^[A-Za-z-]+$/;
+    if(x.length<2 || x==letters) {
+        y.innerHTML="❌" ;
+        
+      }
+      else{
+          y.innerHTML="✔️" ;
+      }
+    //Prénom
+    var x=document.getElementById("name2").value;
+    var y=document.getElementById("jj");
     var letters = /^[A-Za-z]+$/;
-if(x.length<2 || x==letters) {
+if(x.length<3 || x==letters) {
+      y.innerHTML="❌" ;
+      
+    }
+    else{
+        y.innerHTML="✔️" ;
+    }
+    //code posstal
+    var x=document.getElementById("code").value;
+    var y=document.getElementById("ii");
+    var letters = /^[A-Za-z]+$/;
+if(x.length < 5 || x==letters) {
       y.innerHTML="❌" ;
     
     }
     else{
         y.innerHTML="✔️" ;
     }
-    var x=document.getElementById("name2").value;
-    var y=document.getElementById("jj");
-    var letters = /^[A-Za-z]+$/;
-if(x.length<2 || x==letters) {
+
+    //email
+    var x=document.getElementById("email").value;
+    var y=document.getElementById("e");
+    var eml = new RegExp (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@+[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+    if (!eml.test(x)){
       y.innerHTML="❌" ;
+      
     }
     else{
         y.innerHTML="✔️" ;
     }
-    var x=document.getElementById("code").value;
-    var y=document.getElementById("ii");
-    var letters = /^[A-Za-z]+$/;
-if(x.length < 5 || x==letters) {
-      y.innerHTML="❌" ;
-    }
-    else{
-        y.innerHTML="✔️" ;
-    }
+    //mot de passe
     var x=document.getElementById("pass").value;
     var y=document.getElementById("pp");
-    var letters = /^[A-Za-z]+$/;
-if(x.length < 6 || x==letters) {
+    
+if(x.length < 6 || x=="") {
       y.innerHTML="❌" ;
+      
     }
     else{
         y.innerHTML="✔️" ;
     }
-    var x=document.getElementById("cpass").value;
+      
+    //confirmation de mot de pass
+    var a=document.getElementById("pass").value;
+    var b=document.getElementById("cpass").value;
     var y=document.getElementById("cc");
-    var letters = /^[A-Za-z]+$/;
-    var pass = document.getElementById("pass").value;
-if(x.length < 6 && x==letters && x==pass) {
-      y.innerHTML="❌" ;
+  if (a!=b || a=="") {
+            y.innerHTML="❌" ;
     }
     else{
         y.innerHTML="✔️" ;
     }
+
+    //date
+    var x=document.getElementById("date").value;
+    var y=document.getElementById("d");
+    var age=new Date().getFullYear()-new Date(x).getUTCFullYear();
+  if (age < 18 || x == "") {
+    y.innerHTML="❌" ;
+    }
+    else{
+        y.innerHTML="✔️" ;
+    }
+   
+    //pays
+    var x=document.getElementById("pays").value;
+    var y=document.getElementById("py");
+    
+    if(x=="Please select...") {
+        y.innerHTML="❌" ;
+        
+      }
+      else{
+          y.innerHTML="✔️" ;
+      }
+      //genre
+      var x=document.getElementById("gen").value;
+      var y=document.getElementById("g");
+      
+      if(x=="") {
+          y.innerHTML="❌" ;
+          
+        }
+        else{
+            y.innerHTML="✔️" ;
+        }
+      alert('Please completer votre formulaire');
+      return false;
 }
